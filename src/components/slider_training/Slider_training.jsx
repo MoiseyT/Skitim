a7067f24776785cc93a05db83a0226a778e2423b
Timofei Moiseev <moiseevt115@gmail.com>
2024-04-24 13:18:03 +0200

    add training page

--- src/components/slider_training/Slider_training.jsx
+++ src/components/slider_training/Slider_training.jsx
@@ -4,23 +4,29 @@ import Winter from '../../assets/Winter.png';
 import Spring from '../../assets/Spring.png';
 import Summer from '../../assets/Summer.png';
 import Autumn from '../../assets/Autumn.png';
-const Thumbnail = ({ arr, image, index }) => {
-    return (<div className="tumbnail">
-      {
-        arr.map((imgsrc, i) => (
-          <img
-            key={i}
-            height="50"
-            src={imgsrc}
-            onClick={() => image(i)}
-            className={index === i ? 'active' : ''}
-          />
-        ))
-      }
-    </div>)
-  }
 
-const Slideshow = ({ imgs }) => {
+import Winter_icon from '../../assets/Snowflake.png';
+import Spring_icon from '../../assets/flower.png';
+import Summer_icon from '../../assets/Sun.png';
+import Autumn_icon from '../../assets/Blat.png';
+
+const Thumbnail = ({ arr, image, index }) => {
+  return (<div className="tumbnail">
+    {
+      arr.map((imgsrc, i) => (
+        <img
+          key={i}
+          height="50"
+          src={imgsrc}
+          onClick={() => image(i)}
+          className={index === i ? 'active thumbnail-img' : 'thumbnail-img'}
+        />
+      ))
+    }
+  </div>)
+}
+
+const Slideshow = ({ imgs, Icones }) => {
   const [index, setIndex] = useState(0)
 
   useEffect(() => {
@@ -50,7 +56,7 @@ const Slideshow = ({ imgs }) => {
         <button onClick={prev}>&#10094;</button>
         <button onClick={next}>&#10095;</button>
       </div >
-      <Thumbnail arr={imgs} image={setIndex} index={index} />
+      <Thumbnail arr={Icones} image={setIndex} index={index} />
     </div>
 
   )
@@ -58,16 +64,23 @@ const Slideshow = ({ imgs }) => {
 
 
 function Slider_training() {
-    return (
-        <div>
-            <Slideshow imgs={[
-                Winter,
-                Summer,
-                Autumn,
-                Spring
-            ]}
-            />
-        </div>
-    );
+  return (
+    <div>
+      
+      <Slideshow imgs={[
+        Winter,
+        Spring,
+        Summer,
+        Autumn,
+      ]}
+                Icones ={[
+        Winter_icon,
+        Spring_icon, 
+        Summer_icon,
+        Autumn_icon,
+      ]}
+      />
+    </div>
+  );
 };
 export default Slider_training
--- src/page/Training_page.jsx
+++ src/page/Training_page.jsx
@@ -7,14 +7,9 @@ export function Training_page() {
   return (
     <div className='Training_page'>
       <Navbar />
-      <div class="name_level">
-      <h1>Training in the beauteful</h1>
-      </div>
       <Slider_training />
-          <Prices />
+      <Prices />
       <Plan />
-      <div class="name_level">
-      </div>
       <Contacts />
       <Link to={"/"}>Домой</Link>
 
